11f55590e58782ce3c1763edacc3bfac3c649511
Danil <fff.fff1984@mail.ru>
2022-11-16 19:44:41 +0600

    Commit message

--- src/gits.java
+++ src/gits.java
@@ -1,5 +1,8 @@
 public class gits {
     public static void main(String[] args) {
         System.out.println("Введите текст ");
+
+        System.out.println("Введите текст 2");
+        System.out.println("Введите текст 3");
     }
 }
